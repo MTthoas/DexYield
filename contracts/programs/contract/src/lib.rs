f655f5107be22db021f46c0cac1dc6a07c60ebe2
MtThoas <matthias.pecquery@gmail.com>
2025-04-07 12:31:38 +0200

    feat: add counter

--- contracts/programs/contract/src/lib.rs
+++ contracts/programs/contract/src/lib.rs
@@ -3,14 +3,49 @@ use anchor_lang::prelude::*;
 declare_id!("4WC4MDgPpPW3PY4idJT5ffX1NJpkYhLfRv2sDUsGCWLa");
 
 #[program]
-pub mod contract {
+pub mod counter {
     use super::*;
 
     pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
-        msg!("Greetings from: {:?}", ctx.program_id);
+        let counter = &ctx.accounts.counter;
+        msg!("Counter account created! Current count: {}", counter.count);
+        Ok(())
+    }
+
+    // Instruction to increment a counter account
+    pub fn increment(ctx: Context<Increment>) -> Result<()> {
+        // Mutable reference to the counter account from the Increment struct
+        let counter = &mut ctx.accounts.counter;
+        msg!("Previous counter: {}", counter.count);
+
+        // Increment the count value stored on the counter account by 1
+        counter.count = counter.count.checked_add(1).unwrap();
+        msg!("Counter incremented! Current count: {}", counter.count);
         Ok(())
     }
 }
 
 #[derive(Accounts)]
-pub struct Initialize {}
+pub struct Initialize<'info> {
+    #[account(mut)]
+    pub user: Signer<'info>,
+
+    #[account(
+        init,
+        payer = user,
+        space = 8 + 8, // 8 bytes for the account discriminator + 8 bytes for the count
+    )]
+    pub counter: Account<'info, Counter>,
+    pub system_program: Program<'info, System>
+}
+
+#[derive(Accounts)]
+pub struct Increment<'info> {
+    #[account(mut)] // specify account is mutable because we are updating its data
+    pub counter: Account<'info, Counter>, // specify account is 'Counter' type
+}
+
+#[account]
+pub struct Counter {
+    pub count: u64, // 8 bytes for the u64 count
+}
