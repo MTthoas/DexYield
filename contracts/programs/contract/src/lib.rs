f82bc979900b71c362de3a604652a8e86c0ac5e6
MtThoas <matthias.pecquery@gmail.com>
2025-04-08 11:52:18 +0200

    feat(rust): increment / decrement / reset

--- contracts/programs/contract/src/lib.rs
+++ contracts/programs/contract/src/lib.rs
@@ -7,20 +7,34 @@ pub mod counter {
     use super::*;
 
     pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
-        let counter = &ctx.accounts.counter;
+        let counter = &mut ctx.accounts.counter;
+        counter.bump = ctx.bumps.counter; // store bump seed in `Counter` account
         msg!("Counter account created! Current count: {}", counter.count);
+        msg!("Counter bump: {}", counter.bump);
         Ok(())
     }
 
-    // Instruction to increment a counter account
     pub fn increment(ctx: Context<Increment>) -> Result<()> {
-        // Mutable reference to the counter account from the Increment struct
         let counter = &mut ctx.accounts.counter;
         msg!("Previous counter: {}", counter.count);
-
-        // Increment the count value stored on the counter account by 1
         counter.count = counter.count.checked_add(1).unwrap();
         msg!("Counter incremented! Current count: {}", counter.count);
+        Ok(())
+    }
+
+    pub fn decrement(ctx: Context<Increment>) -> Result<()> {
+        let counter = &mut ctx.accounts.counter;
+        msg!("Previous counter: {}", counter.count);
+        counter.count = counter.count.checked_sub(1).unwrap();
+        msg!("Counter decremented! Current count: {}", counter.count);
+        Ok(())
+    }
+
+    pub fn reset(ctx: Context<Increment>) -> Result<()> {
+        let counter = &mut ctx.accounts.counter;
+        msg!("Previous counter: {}", counter.count);
+        counter.count = 0;
+        msg!("Counter reset! Current count: {}", counter.count);
         Ok(())
     }
 }
@@ -30,22 +44,51 @@ pub struct Initialize<'info> {
     #[account(mut)]
     pub user: Signer<'info>,
 
+    // Create and initialize `Counter` account using a PDA as the address
     #[account(
         init,
+        seeds = [b"counter"], 
+        bump,          
         payer = user,
-        space = 8 + 8, // 8 bytes for the account discriminator + 8 bytes for the count
+        space = 8 + Counter::INIT_SPACE
     )]
     pub counter: Account<'info, Counter>,
-    pub system_program: Program<'info, System>
+    pub system_program: Program<'info, System>,
 }
 
 #[derive(Accounts)]
 pub struct Increment<'info> {
-    #[account(mut)] // specify account is mutable because we are updating its data
-    pub counter: Account<'info, Counter>, // specify account is 'Counter' type
+    #[account(
+        mut,
+        seeds = [b"counter"], 
+        bump = counter.bump, 
+    )]
+    pub counter: Account<'info, Counter>,
+}
+
+#[derive(Accounts)]
+pub struct Decrement<'info> {
+    #[account(
+        mut,
+        seeds = [b"counter"], 
+        bump = counter.bump, 
+    )]
+    pub counter: Account<'info, Counter>,
+}
+
+#[derive(Accounts)]
+pub struct Reset<'info> {
+    #[account(
+        mut,
+        seeds = [b"counter"], 
+        bump = counter.bump, 
+    )]
+    pub counter: Account<'info, Counter>,
 }
 
 #[account]
+#[derive(InitSpace)]
 pub struct Counter {
-    pub count: u64, // 8 bytes for the u64 count
+    pub count: u64, // 8 bytes
+    pub bump: u8,   // 1 byte
 }
--- contracts/tests/counter.ts
+++ contracts/tests/counter.ts
@@ -1,7 +1,7 @@
 import * as anchor from "@coral-xyz/anchor";
 import { Program } from "@coral-xyz/anchor";
 import { Counter } from "../target/types/counter";
-import { Keypair } from "@solana/web3.js";
+import { PublicKey } from "@solana/web3.js";
 
 describe("counter", () => {
   // Configure the client to use the local cluster.
@@ -10,41 +10,66 @@ describe("counter", () => {
 
   const program = anchor.workspace.Counter as Program<Counter>;
 
-  // Generate a new keypair to use as the address the counter account
-  const counterAccount = new Keypair();
+  const [counterPDA] = PublicKey.findProgramAddressSync(
+    [Buffer.from("counter")],
+    program.programId
+  );
 
   it("Is initialized!", async () => {
-    // Invoke the initialize instruction
-    const transactionSignature = await program.methods
-      .initialize()
-      .accounts({
-        counter: counterAccount.publicKey,
-      })
-      .signers([counterAccount]) // include counter keypair as additional signer
-      .rpc({ skipPreflight: true });
+    try {
+      const txSig = await program.methods
+        .initialize()
+        .accounts({
+          counter: counterPDA,
+        })
+        .rpc();
 
-    // Fetch the counter account data
-    const accountData = await program.account.counter.fetch(
-      counterAccount.publicKey
-    );
+      const accountData = await program.account.counter.fetch(counterPDA);
+      console.log(`Transaction Signature: ${txSig}`);
+      console.log(`Count: ${accountData.count}`);
+    } catch (error) {
+      // If PDA Account already created, then we expect an error
+      console.log(error);
+    }
+  });
+
+  it("Increment", async () => {
+    const transactionSignature = await program.methods
+      .increment()
+      .accounts({
+        counter: counterPDA,
+      })
+      .rpc();
+
+    const accountData = await program.account.counter.fetch(counterPDA);
 
     console.log(`Transaction Signature: ${transactionSignature}`);
     console.log(`Count: ${accountData.count}`);
   });
 
-  it("Increment", async () => {
-    // Invoke the increment instruction
+  it("Decrement", async () => {
     const transactionSignature = await program.methods
-      .increment()
+      .decrement()
       .accounts({
-        counter: counterAccount.publicKey,
+        counter: counterPDA,
       })
       .rpc();
 
-    // Fetch the counter account data
-    const accountData = await program.account.counter.fetch(
-      counterAccount.publicKey
-    );
+    const accountData = await program.account.counter.fetch(counterPDA);
+
+    console.log(`Transaction Signature: ${transactionSignature}`);
+    console.log(`Count: ${accountData.count}`);
+  });
+
+  it("Reset", async () => {
+    const transactionSignature = await program.methods
+      .reset()
+      .accounts({
+        counter: counterPDA,
+      })
+      .rpc();
+
+    const accountData = await program.account.counter.fetch(counterPDA);
 
     console.log(`Transaction Signature: ${transactionSignature}`);
     console.log(`Count: ${accountData.count}`);
